a63165af5103c85a5a98336fbf4620412e6c7cc6
YunusPanwar70 <yunuspanwar78@gmail.com>
2025-10-09 18:43:17 +0530

    DB change

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -1,34 +1,38 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres",
-    protocol: "postgres",
-    logging: false,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false, // needed for Render
-        },
-    },
-});
+// Create Sequelize instance
+export const sequelize = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        logging: false, // ab SQL queries console me dikhenge
+    }
+);
 
+// Check DB connection
 export const checkDbConnection = async () => {
     try {
         await sequelize.authenticate();
-        console.log("✅ Postgres connected successfully");
+        console.log('Postgres connected successfully');
     } catch (error) {
-        console.error("❌ Failed to connect Postgres:", error);
+        console.error('Failed to connect Postgres', error);
     }
 };
 
+// Sync DB (create table if not exists)
 export const syncDatabase = async () => {
     try {
         await sequelize.sync({ alter: true });
-        console.log("✅ Database synced successfully");
+        // alter = update structure automatically
+        console.log('Database synced successfully');
     } catch (error) {
-        console.error("❌ Error syncing database:", error);
+        console.error('Error syncing database:', error);
     }
 };
